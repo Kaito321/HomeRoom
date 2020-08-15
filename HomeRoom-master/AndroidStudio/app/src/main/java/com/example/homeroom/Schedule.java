1f853a0c00bc595b7b71327dbe90704df8b3b7d8
Kaito <rars213@gmail.com>
2020-08-15 15:26:59 -0500

    Added button
    Added button to schedule page. No real functionality available yet

--- HomeRoom-master/AndroidStudio/app/src/main/java/com/example/homeroom/Schedule.java
+++ HomeRoom-master/AndroidStudio/app/src/main/java/com/example/homeroom/Schedule.java
@@ -3,6 +3,8 @@ package com.example.homeroom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Schedule extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Schedule extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+    }
+
+    public void goBack(View view){
+        Button backButton;
+        
     }
 }
