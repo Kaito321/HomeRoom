2da5ca490b8afee87b02b80a96a9365ac0fe150c
Kaito <rars213@gmail.com>
2020-08-15 12:41:27 -0500

    Made Activities
    Created activity files for login, home, shced, profile, and mesage as well as ocmputer availability

---
+++ HomeRoom-master/AndroidStudio/app/src/main/java/com/example/homeroom/ComputerAvailability.java
@@ -0,0 +1,14 @@
+package com.example.homeroom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComputerAvailability extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_computer_availability);
+    }
+}
