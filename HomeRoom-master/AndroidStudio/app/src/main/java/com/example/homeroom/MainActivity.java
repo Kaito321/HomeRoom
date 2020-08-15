14620543c7bce386fa42066609b429aa2fbc1655
Kaito <rars213@gmail.com>
2020-08-15 17:29:08 -0500

    Transition Main to Login
    set up button to transition from main to the login page

--- HomeRoom-master/AndroidStudio/app/src/main/java/com/example/homeroom/MainActivity.java
+++ HomeRoom-master/AndroidStudio/app/src/main/java/com/example/homeroom/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.homeroom;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -11,16 +12,29 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button = (Button)findViewById(R.id.LogInButton);
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogIn();
+            }
+        });
+    }
 
+    public void openLogIn(){
+        Intent intent = new Intent(this, LogIn.class);
+        startActivity(intent);
     }
 
     @Override
