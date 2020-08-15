32ceaefc1e19ea2c844db83d1460e1ab76f79eed
ArrsDesign <Hack&2098>
2020-08-15 15:52:20 -0500

    Updated color added date
    updated color added date to home

--- HomeRoom-master/AndroidStudio/app/src/main/java/com/example/homeroom/Home.java
+++ HomeRoom-master/AndroidStudio/app/src/main/java/com/example/homeroom/Home.java
@@ -3,6 +3,10 @@ package com.example.homeroom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Current date Format for Home/
+        Calendar calendar = Calendar.getInstance();
+        String currentDate = DateFormat.getDateInstance(DateFormat.FULL).format(calendar.getTime());
+
+        //Referencing TextView from xml/
+        TextView textViewDate = findViewById(R.id.textViewDate)
+                textViewDate.setText(currentDate);
+
+
     }
 }
